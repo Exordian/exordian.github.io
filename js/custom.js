4f8ddbc6c93f033766d59e00cd73881d08a62dc8
Jakob Englisch <jakob.englisch@catalysts.cc>
2014-12-12 20:17:26 +0100

    login, register, user profile page

---
+++ js/custom.js
@@ -0,0 +1,79 @@
+var loggedIn = false;
+var username = '';
+var email = '';
+var place = '';
+var phone = '';
+var birthyear = '';
+
+function defaultProfileData() {
+    username = '';
+    email = 'mustername@musterdomain.com';
+    place = 'Musterort';
+    phone = '0664/1234567';
+    birthyear = '1981';
+}
+
+function login() {
+    loggedIn = true;
+    $('#loginLink').hide();
+    $('#logoutLink').show();
+    $('#userLink').text(username);
+    $('#userLink').show();
+    $('#registerLink').hide();
+}
+
+function logout() {
+    loggedIn = false;
+    username = '';
+    $('#loginLink').show();
+    $('#logoutLink').hide();
+    $('#userLink').hide();
+    $('#registerLink').show();
+    $('#contentInclude').load("partials/main.html");
+}
+
+function loadUserProfile() {
+    $('#contentInclude').load("partials/profil.html", function(){
+        $('#profileUsername').text(username);
+        $('#profileMail').text(email);
+        $('#profilePlace').text(place);
+        $('#profilePhone').text(phone);
+        $('#profileBirthyear').text(birthyear);
+    });
+}
+
+ $(document).ready( function() {
+    /* on page load */
+    $(function(){
+        $('#contentInclude').load("partials/main.html");
+        $('#loginInclude').load("partials/anmelden-modal.html");
+        $('#registerInclude').load("partials/register-modal.html");
+        logout();
+        defaultProfileData();
+    });
+
+    /* On-Click-Handler */
+    $(document).on ('click', '#registerBtn', function () {
+        username = $('#register-username').val();
+        email = $('#register-email').val();
+        place = $('#register-place').val();
+        phone = $('#register-phone').val();
+        birthyear = $('#register-birthyear').val();
+        login();
+        loadUserProfile();
+        $('#register').hide();
+    });
+    $(document).on ('click', '#loginBtn', function () {
+        defaultProfileData();
+        username = $('#login-username').val();
+        login();
+        $('#login').hide();
+    });
+    $(document).on ('click', '#logoutLink', function () {
+        defaultProfileData();
+        logout();
+    });
+    $(document).on ('click', '#userLink', function () {
+        loadUserProfile();
+    });
+});
